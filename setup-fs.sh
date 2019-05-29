5c9cd4765a49f7f8d8534b4fc3f35b17f5838213
mbargiel <mbargiel@btopservice.net>
2019-05-29 21:57:51 +0200

    moved configs

--- setup-fs.sh
+++ setup-fs.sh
@@ -3,6 +3,6 @@
 mkdir -p /media/nas
 mkdir -p /media/nas/video/{anime,movies,transcoding,torrents}
 mkdir -p /media/nas/video/torrents/{downloading,backups,watch,completed}
-mkdir -p /media/nas/raspberry/{plex,jackett,sonarr,transmission}/config
+mkdir -p /usr/local/bin/rpi-media-as-code/{plex,jackett,sonarr,transmission}/config
 
 echo "Initialized missing directories"
