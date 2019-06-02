d161d6618bb8e0f9e5389c3cb7d9b3df51652e1e
mbargiel <mbargiel@btopservice.net>
2019-06-02 17:10:43 +0200

    Moved plex to the NAS

--- setup-fs.sh
+++ setup-fs.sh
@@ -6,3 +6,4 @@ mkdir -p /media/nas/video/torrents/{downloading,backups,watch,completed}
 mkdir -p /usr/local/bin/rpi-media-as-code/{jackett,sonarr,transmission}/config
 
 echo "Initialized missing directories"
+git 
