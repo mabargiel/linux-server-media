59afe5b3f70c9a69ee130ef27fbf26a5dc0d9eda
mbargiel <mbargiel@btopservice.net>
2020-03-10 10:57:24 +0100

    Shortened paths

--- setup-fs.sh
+++ setup-fs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir -p /media/nas
-mkdir -p /media/nas/video/{anime,movies,transcoding,torrents}
-mkdir -p /media/nas/video/torrents/{downloading,backups,watch,completed}
+mkdir -p /media/nas/{anime,movies,transcoding,torrents}
+mkdir -p /media/nas/torrents/{downloading,backups,watch,completed}
 mkdir -p /usr/local/bin/rpi-media-as-code/{jackett,sonarr,transmission}/config
 
 echo "Initialized missing directories"
