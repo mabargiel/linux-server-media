4fd9c243d988bb862eca2e603810bb683af7071d
mbargiel <31691087+mabargiel@users.noreply.github.com>
2021-01-04 09:49:00 +0100

    Update setup-fs.sh

--- setup-fs.sh
+++ setup-fs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir -p /usr/local/bin/media/torrents/{watch,downloading}
 mkdir -p /usr/local/bin/media/{jackett,sonarr,transmission}/config
 
 echo "Initialized missing directories"
