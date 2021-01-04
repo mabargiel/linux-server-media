42e6073af53fc2db91dbcc1de3b5812959ca728c
mbargiel <31691087+mabargiel@users.noreply.github.com>
2021-01-04 09:47:12 +0100

    Update setup-fs.sh

--- setup-fs.sh
+++ setup-fs.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-mkdir -p /media/nas
-mkdir -p /media/nas/{anime,movies,transcoding,torrents}
-mkdir -p /media/nas/torrents/{downloading,backups,watch,completed}
-mkdir -p /usr/local/bin/rpi-media-as-code/{jackett,sonarr,transmission}/config
+mkdir -p /usr/local/bin/media/{jackett,sonarr,transmission}/config
 
 echo "Initialized missing directories"
